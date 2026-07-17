b61f329c64b33695a8acb9b4d7e0790cd9bf089a
djaiz <djaizmassi@gmail.com>
2026-07-17 12:48:50 -0400

    Add release and emplace functions to PointerArray

--- include/Core/Container/PointerArray.h
+++ include/Core/Container/PointerArray.h
@@ -1,14 +1,23 @@
 #pragma once
 
+#include <memory>
+
 #include "Core/Container/Array.h"
 #include "Core/Util/Macros.h"
 
 namespace ml {
 
+/**
+ * A container for an array of owned pointers
+ * @tparam T The type of object pointed to
+ */
 template <typename T>
+requires (!std::is_pointer_v<T>)
 class PointerArray final : public ml::Container<PointerArray<T>, T*> {
     NO_COPY(PointerArray)
 public:
+    static constexpr uint64_t InvalidIndex = ml::Array<T>::InvalidIndex;
+
     explicit PointerArray() = default;
 
     explicit PointerArray(uint64_t size) {
@@ -36,6 +45,16 @@ public:
     void set(uint64_t index, T* ptr) {
         delete mArray[index];
         mArray[index] = ptr;
+    }
+
+    template <typename ...Args>
+    void emplace(uint64_t index, Args&&... args) {
+        delete mArray[index];
+        mArray[index] = new T(std::forward<Args>(args)...);
+    }
+
+    void emplaceNew(uint64_t index) requires (std::is_default_constructible_v<T>) {
+        this->set(index, new T());
     }
 
     bool replace(T* oldPtr, T* newPtr) {
@@ -71,10 +90,39 @@ public:
         return false;
     }
 
+    std::unique_ptr<T> release(uint64_t index) {
+        T* item = mArray[index];
+        mArray[index] = nullptr;
+        return std::unique_ptr<T>(item);
+    }
+
+    std::unique_ptr<T> release(T* item) {
+        for (T*& ptr : mArray) {
+            if (ptr == item) {
+                ptr = nullptr;
+                return std::unique_ptr<T>(item);
+            }
+        }
+
+        return nullptr;
+    }
+
     ~PointerArray() {
         for (T* ptr : mArray) {
             delete ptr;
         }
+    }
+
+    bool isInBounds(uint64_t index) const {
+        return mArray.isInBounds(index);
+    }
+
+    T* safeAt(uint64_t index) {
+        if (index >= mArray.size()) {
+            return nullptr;
+        }
+
+        return mArray[index];
     }
 
     T** begin() { return mArray.begin(); }
